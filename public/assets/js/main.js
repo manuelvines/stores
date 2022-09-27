cf4c2f4c78cd6d4fd7b0131dff00c9da4febab6c
manuelvines <cacvmanu@gmai.com>
2022-09-27 14:48:06 -0500

    Peticiión Ajax de crear storie funciona OK pero esta en index.php debemos cerar controllador y clase para Tiendas

---
+++ public/assets/js/main.js
@@ -0,0 +1,38 @@
+jQuery("#crud_stores_form_create").submit(function(e){
+      e.preventDefault();
+        
+      console.log("desde el backend");
+      /*
+      jQuery.ajax({
+            url: ajax_var.url,
+            type: "POST", // podría ser get, post, put o delete.
+            data: jQuery(this).serialize(),
+        beforeSend: function() {
+            alert("voy enviar información")
+        },
+        success: function ($response) {
+         console.log($response);
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.n' + jqXHR.responseText;
+            }
+            alert(msg);
+        },
+    });
+    */
+
+});
---
+++ public/assets/views/store/crud.php
@@ -0,0 +1,39 @@
+<?php ob_start(); ?>
+  <div class="row">
+     <div class="col-xs-12">
+
+            <h2>INGRESA LA UBICACIÓN</h2>
+            
+            <form id="crud_stores_form_create"  action="" method="POST">
+
+                
+                
+                <input type="text" name="method" value="POST">
+                <input type="text" name="action" value="create-store">
+                <input type="text" name="_wpnonce" value="<?php echo wp_create_nonce('create-store'); ?>">
+
+
+                <div class="form-group">
+                    <input type="text"  name="store_name" class="form-control" placeholder="Nombre">
+                </div>
+                <div class="form-group">
+                    <input type="text"  name="store_address"  class="form-control" placeholder="Dirección">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="store_latitud"  class="form-control" placeholder="Latitud">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="store_longitud"  class="form-control" placeholder="Longitud">
+                </div>
+                <div class="form-group">
+               
+                    <input type="submit" class="btn btn-success">
+
+                </div>
+
+            </form>
+
+     </div>
+  </div>
+<?php  $view_crud_store = ob_get_contents(); ?>
+<?php ob_end_clean(); ?>
